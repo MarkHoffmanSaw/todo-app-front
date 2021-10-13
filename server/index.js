69922ee261ed4cfd6aeeb432784c4b9d2b523a96
Artur <tullers@mail.ru>
2021-10-13 17:54:08 +0300

    upd

--- server/index.js
+++ server/index.js
@@ -1,10 +1,37 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const pool = require("./db");
 
 // middleware
 app.use(cors());
 app.use(express.json());
+
+//////////////////// *** Routes *** ///////////////////
+
+// Create a project
+
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES($1)",
+      [description]
+    );
+
+    res.json(newTodo);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// Get all projects
+
+// Get a project
+
+// Update a project
+
+// Delete a project
 
 app.listen(5000, () => {
   console.log("Server has started on port 5000");
